42594a47e6b694f616f6659430c4895a56ddc00e
Roben Kleene <robenk@gmail.com>
2015-07-06 00:40:05 -0400

    Work on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
-
+var jade = require('gulp-jade');
+var path = require('path');
+var plumber = require('gulp-plumber');
 
 var paths = {
   example: {
@@ -8,12 +10,34 @@ var paths = {
   }
 };
 
+paths.example.jade = {};
+paths.example.jade.src = path.join(paths.example.src, 'jade');
+paths.example.jade.build = path.join(paths.test.build, 'html');
+paths.example.jade.srcGlob = path.join(paths.example.jade.src, '/*.jade');
+paths.example.jade.srcWatchGlob = path.join(paths.example.jade.src, '/**/*.jade');
+
 
 gulp.task('jade-example', function() {
-  gulp.src(path.join(paths.example.src, 'jade/index.jade'))
+  gulp.src(path.join(paths.example.src, 'jade/*.jade'))
     .pipe(plumber())
     .pipe(jade({
       pretty: true
     }))
     .pipe(gulp.dest(paths.example.build));
 });
+
+gulp.task('watch', function() {
+    // Test
+    gulp.watch(paths.test.src + 'jade/*.*', ['jade-test']);
+
+    var testJSPaths = [paths.test.src + 'tests/*.js', paths.test.src + 'js/*.js'];
+    gulp.watch(testJSPaths, ['browserify-test']);
+
+    // Example
+    // When "example" files change, also build "test" because "test" imports "example" content
+    var exampleJadePaths = [paths.example.src + 'jade/*.*', paths.example.src + 'markdown/*.*'];
+    gulp.watch(exampleJadePaths, ['jade-example', 'jade-test']);
+});
+
+gulp.task('jade', ['jade-test', 'jade-example']);
+gulp.task('default', ['jade', 'browserify-test']);
